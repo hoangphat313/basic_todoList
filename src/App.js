ef2d1d96365bcebce395bf158332713bf2d8d883
hoangphat313 <phoangphat313@gmail.com>
2024-07-02 23:02:12 +0700

    v2

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
+import { useState } from "react";
+
 
 function App() {
+  const [value, setValue] = useState('')
+
   return (
-    <div className="App">
-      app
+    <div className=" flex gap-4 h-screen justify-center items-center">
+      <input type="text" className=" outline-none border border-blue-600 px-4 py-2 w-[400px] "
+        value={value}
+        onChange={e => setValue(e.target.value)}
+      />
+      <button
+        type="button"
+        className=" outline-none px-4 py-2 bg-blue-500 rounded-md text-white "
+      >
+        Add
+      </button>
     </div>
   );
 }
